a96c947a562be805f9991330ce855fcbc7a17b20
長町俊吾 <nagamaaaaachi@gmail.com>
2020-07-19 17:29:07 +0900

    frontend first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VueApollo from 'vue-apollo';
+import ApolloClient from 'apollo-boost';
 
 Vue.config.productionTip = false;
 
+const apolloClient = new ApolloClient({
+	uri: 'http://localhost:5000/graphql',
+});
+
+const apolloProvider = new VueApollo({
+	defaultClient: apolloClient,
+});
+
+Vue.use(VueApollo);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	apolloProvider,
+	render: (h) => h(App),
+}).$mount('#app');
---
+++ src/views/GraphQL.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>GraphQL</h1>
+    <ul v-for="post in allPosts" :key="post.id">
+      <li>タイトル： {{post.title}}</li>
+      <li>内容： {{post.content}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+const POSTS_QUERY = gql`
+  query allPosts {
+    allPosts {
+      title
+      content
+    }
+  }
+`;
+
+export default {
+  name: "index",
+  data() {
+    return {
+      allPosts: []
+    };
+  },
+  apollo: {
+    allPosts: {
+      query: POSTS_QUERY
+    }
+  }
+};
+</script>
