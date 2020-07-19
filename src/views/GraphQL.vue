23dcf2227bf57c06e5030b8af4eb392f0f54c320
長町俊吾 <nagamaaaaachi@gmail.com>
2020-07-19 22:43:50 +0900

    modify sample-page

--- src/views/GraphQL.vue
+++ src/views/GraphQL.vue
@@ -1,36 +1,37 @@
 <template>
-  <div>
-    <h1>GraphQL</h1>
-    <ul v-for="post in allPosts" :key="post.id">
-      <li>タイトル： {{post.title}}</li>
-      <li>内容： {{post.content}}</li>
-    </ul>
-  </div>
+	<div>
+		<h1>GraphQL</h1>
+		<ul v-for="post in allPosts" :key="post.id">
+			<li>TITLE: {{ post.title }}</li>
+			<li>CONTENT: {{ post.content }}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 const POSTS_QUERY = gql`
-  query allPosts {
-    allPosts {
-      title
-      content
-    }
-  }
+	query {
+		allPosts {
+			id
+			title
+			content
+		}
+	}
 `;
 
 export default {
-  name: "index",
-  data() {
-    return {
-      allPosts: []
-    };
-  },
-  apollo: {
-    allPosts: {
-      query: POSTS_QUERY
-    }
-  }
+	name: 'index',
+	data() {
+		return {
+			allPosts: [],
+		};
+	},
+	apollo: {
+		allPosts: {
+			query: POSTS_QUERY,
+		},
+	},
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			port: 8888,
 			proxy: {
 				'/graphql': {
-					target: 'http://localhost:5000', // local api server
+					target: 'http://localhost:4000', // local api server
 				},
 			},
 		},
