38e69312af1c07185f54b23adb37d341f1d4f2fe
長町俊吾 <nagamaaaaachi@gmail.com>
2020-08-15 03:02:25 +0900

    add re-serach after regist

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,125 +1,128 @@
 <template>
-	<div>
-		<section>
-			<div class="columns">
-				<!-- DatePicker -->
-				<div class="column">
-					<b-field label="DATE">
-						<b-datepicker
-							placeholder="Click to select..."
-							icon="calendar-today"
-							trap-focus
-							v-model="date"
-						></b-datepicker>
-					</b-field>
-				</div>
-				<!-- Page Number -->
-				<div class="column">
-					<b-field label="PAGE">
-						<b-numberinput v-model="page" step="5" :min="min" :max="max"></b-numberinput>
-					</b-field>
-				</div>
-			</div>
-			<div class="columns">
-				<!-- Register Button -->
-				<div class="column">
-					<div class="is-pulled-right">
-						<b-button type="is-info" icon-left="book-open" @click="regist">I Read it !</b-button>
-					</div>
-				</div>
-			</div>
-		</section>
-	</div>
+  <div>
+    <section>
+      <div class="columns">
+        <!-- DatePicker -->
+        <div class="column">
+          <b-field label="DATE">
+            <b-datepicker
+              placeholder="Click to select..."
+              icon="calendar-today"
+              trap-focus
+              v-model="date"
+            ></b-datepicker>
+          </b-field>
+        </div>
+        <!-- Page Number -->
+        <div class="column">
+          <b-field label="PAGE">
+            <b-numberinput v-model="page" step="5" :min="min" :max="max"></b-numberinput>
+          </b-field>
+        </div>
+      </div>
+      <div class="columns">
+        <!-- Register Button -->
+        <div class="column">
+          <div class="is-pulled-right">
+            <b-button type="is-info" icon-left="book-open" @click="regist">I Read it !</b-button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 const REGISTER_INFO_QUERY = gql`
-	query {
-		book(id: 1) {
-			id
-			allPages
-			progress {
-				id
-				currentPage
-				readAt
-			}
-		}
-	}
+  query {
+    book(id: 1) {
+      id
+      allPages
+      progress {
+        id
+        currentPage
+        readAt
+      }
+    }
+  }
 `;
 
 const PROGRESS_REGISTER_MUTATION = gql`
-	mutation createProgress($currentPage: Int!, $readAt: ISO8601DateTime!) {
-		createProgress(bookId: 1, currentPage: $currentPage, readAt: $readAt) {
-			id
-			book {
-				id
-				title
-			}
-			currentPage
-			readAt
-		}
-	}
+  mutation createProgress($currentPage: Int!, $readAt: ISO8601DateTime!) {
+    createProgress(bookId: 1, currentPage: $currentPage, readAt: $readAt) {
+      id
+      # currentPage
+      # readAt
+      # book {
+      #   id
+      #   title
+      # }
+    }
+  }
 `;
 
 export default {
-	name: 'RegisterForm',
+  name: "RegisterForm",
 
-	props: {},
+  props: {},
 
-	data() {
-		return {
-			book: {},
-			date: new Date(),
-			page: 0,
-			min: 0,
-			max: 500,
-		};
-	},
+  data() {
+    return {
+      book: {},
+      date: new Date(),
+      page: 0,
+      min: 0,
+      max: 500
+    };
+  },
 
-	apollo: {
-		book: {
-			query: REGISTER_INFO_QUERY,
-		},
-	},
+  apollo: {
+    book: {
+      query: REGISTER_INFO_QUERY
+    }
+  },
 
-	async mounted() {
-		await this.$apollo.queries.book.refetch();
-		this.setInitValues();
-	},
+  async mounted() {
+    await this.$apollo.queries.book.refetch();
+    this.setInitValues();
+  },
 
-	methods: {
-		setInitValues() {
-			const progress = this.book.progress;
-			if (!progress || progress.length === 0) return;
+  methods: {
+    setInitValues() {
+      const progress = this.book.progress;
+      if (!progress || progress.length === 0) return;
 
-			// page, min
-			const idx = progress.length - 1;
-			this.page = progress[idx].currentPage;
-			this.min = progress[idx].currentPage;
+      // page, min
+      const idx = progress.length - 1;
+      this.page = progress[idx].currentPage;
+      this.min = progress[idx].currentPage;
 
-			// max
-			this.max = this.book.allPages;
-		},
+      // max
+      this.max = this.book.allPages;
+    },
 
-		async regist() {
-			// TODO: input-numberのバリデーションチェックを行う
+    async regist() {
+      // TODO: date, pageのバリデーションチェックを行う
 
-			const result = await this.$apollo.mutate({
-				mutation: PROGRESS_REGISTER_MUTATION,
-				variables: {
-					currentPage: this.page,
-					readAt: this.date,
-				},
-			});
-			console.log(`Result of Mutation: ${result}`);
+      await this.$apollo.mutate({
+        mutation: PROGRESS_REGISTER_MUTATION,
+        variables: {
+          currentPage: this.page,
+          readAt: this.date
+        }
+      });
 
-			this.$buefy.toast.open({
-				message: 'Register Success!',
-				type: 'is-success',
-			});
-		},
-	},
+      this.$buefy.toast.open({
+        message: "Register Success!",
+        type: "is-success"
+      });
+
+      // 再検索
+      await this.$apollo.queries.book.refetch();
+      this.setInitValues();
+    }
+  }
 };
 </script>
